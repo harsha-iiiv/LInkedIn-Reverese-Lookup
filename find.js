1dcf1eebf811ef5aa42a616158e2c303a0d96cc6
Harshavardhan Surisetty <201752039@iiitvadodara.ac.in>
2020-09-14 22:45:30 +0530

    Include email in data

--- find.js
+++ find.js
@@ -4,7 +4,9 @@ const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const perf = require("execution-time")();
 
-const { convertArrayToCSV } = require("convert-array-to-csv");
+const {
+  convertArrayToCSV
+} = require("convert-array-to-csv");
 puppeteer.use(StealthPlugin());
 var indicate = "starting";
 console.log(indicate);
@@ -16,14 +18,15 @@ var head = keys.head;
 
 var browser;
 var page;
+
 var profiles = [];
 indicate = "got all keys";
 console.log(indicate);
 async function lilookup() {
   try {
-    indicate = head
-      ? "lauching browser headless(No GUI)"
-      : "lauching browser normal";
+    indicate = head ?
+      "lauching browser headless(No GUI)" :
+      "lauching browser normal";
     console.log(indicate);
     browser = await puppeteer.launch({
       headless: head,
@@ -93,10 +96,15 @@ async function lilookup() {
       await page.waitForSelector('i[data-icon-name="Down"]');
       await page.click('i[data-icon-name="Down"]');
       console.log("wait for page load for 2 seconds");
-      var seconds = 0;
 
       await page.waitFor(1000);
+      var mail = await page.evaluate(async () => {
+        email = document.querySelector('[data-log-name="PersonName"]')
+          .innerText;
 
+        return email;
+      });
+      var Email = mail;
       try {
         await page.setDefaultTimeout(9000);
         await page.waitForSelector('button[data-content="LinkedIn"]');
@@ -111,9 +119,13 @@ async function lilookup() {
         }
       }
       await page.waitForSelector('button[data-content="LinkedIn"]');
-      await page.click('button[data-content="LinkedIn"]');
+      await page.click('button[data-content="LinkedIn"]', {
+        waitUntil: ["load", "domcontentloaded", "networkidle0", "networkidle2"],
+      });
 
-      await page.waitFor(1000);
+      // await page.waitFor(4000);
+      // await page.waitForXPath('//*[@id="app"]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div/div/div[2]/section/div[2]/div[2]')
+      // await page.waitForXPath('//*[@id="app"]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div/div/div[2]/section/div[2]/div')
       console.log("scrapping starts");
       var data = await page.evaluate(async (i) => {
         var items = document.querySelectorAll('[class^="linkedInContainer"]');
@@ -134,21 +146,14 @@ async function lilookup() {
             .forEach(itrt);
 
           function itrt1(ele) {
-            body.Experience = ele.innerText;
+            body.Experience =
+              typeof ele != "undefined" && ele != null ?
+              ele.innerText :
+              "Nothing in Experience";
           }
-          let exp = document.evaluate(
-            '//*[@id="app"]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div/div/div[2]/section/div[2]/div',
-            document,
-            null,
-            XPathResult.FIRST_ORDERED_NODE_TYPE,
-            null
-          ).singleNodeValue;
-          body.Experience =
-            typeof exp != "undefined" && exp != null ? exp.innerText : "No";
+
           document
-            .querySelectorAll(
-              "#app > div > div._3KAPMPOz8KsW24ooeUflK2 > div > div._1tBzHZ8MrgkSGERLARLRcr > div > div._22cWmNFrfENd4pgCIpPd7L > div > div._3JEfCgO-Y8O01VOuTuqkg7.T7zfU9y-9vwh9A2LcScnN > div > div > div > div > div.customScrollBar.embeddedContent-249 > section > div.contentContainer-415 > div:nth-child(2)"
-            )
+            .querySelectorAll('[class^="contentContainer"]')
             .forEach(itrt1);
         } else {
           console.log("No linked ac found");
@@ -157,7 +162,14 @@ async function lilookup() {
         return body;
       }, i);
       console.log("scrapping ends");
-      profiles.push(data);
+
+      profiles.push({
+        SNo: data.SNo,
+        Email: Email,
+        Head: data.Head,
+        Experience: data.Experience,
+      });
+      // console.log(data);
       const csvFromArrayOfObjects = convertArrayToCSV(profiles);
       fs.writeFile(
         "LinkedInprofiles.csv",
